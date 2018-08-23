615e737953cd56b6f4de991803e3761dff6c0e74
Tinzin <tiffany.tinzin@gmail.com>
2018-08-22 17:30:00 -0700

    clean up "examplemod" shit

--- src/main/kotlin/net/tinzin/relocate/TileEntityExample.kt
+++
@@ -1,6 +0,0 @@
-package net.tinzin.relocate
-
-import net.minecraft.tileentity.TileEntity
-import net.minecraft.tileentity.TileEntityType
-
-class TileEntityExample constructor(type: TileEntityType<*> = ExampleMod.EXAMPLE_TE) : TileEntity(type)
