bb1a817a565f758f6ec6dae45d317085ae3364ff
Tinzin <tiffany.tinzin@gmail.com>
2018-08-23 21:14:27 -0700

    add textures and stuff

--- src/main/kotlin/net/tinzin/relocate/items/Springboard.kt
+++ src/main/kotlin/net/tinzin/relocate/items/Springboard.kt
@@ -4,6 +4,8 @@ import net.minecraft.item.Item
 import net.minecraft.entity.player.EntityPlayer
 
 class Springboard : Item {
+    val VELOCITY = 3.0 // arcane speed units
+
     constructor(builder: Item.Builder) : super(builder) {}
 
     override fun onItemUse(context : net.minecraft.item.ItemUseContext): net.minecraft.util.EnumActionResult{
@@ -12,7 +14,7 @@ class Springboard : Item {
         if (player == null){return net.minecraft.util.EnumActionResult.FAIL} // check for null
 
         if (player.onGround){
-            player.addVelocity(0.0,2.0,0.0) // wheeeee
+            player.addVelocity(0.0,VELOCITY,0.0) // wheeeee
             return net.minecraft.util.EnumActionResult.SUCCESS
         }
         return net.minecraft.util.EnumActionResult.PASS
