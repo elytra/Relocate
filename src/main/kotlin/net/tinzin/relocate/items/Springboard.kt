d53312bfc5c871d9ecb0434da6011146c0925685
Tinzin <tiffany.tinzin@gmail.com>
2018-08-23 22:48:00 -0700

    springboard fully functional now i think

--- src/main/kotlin/net/tinzin/relocate/items/Springboard.kt
+++ src/main/kotlin/net/tinzin/relocate/items/Springboard.kt
@@ -2,6 +2,8 @@ package net.tinzin.relocate
 
 import net.minecraft.item.Item
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.SoundEvents
+import net.minecraft.util.SoundEvent
 
 class Springboard : Item {
     val VELOCITY = 3.0 // arcane speed units
@@ -14,8 +16,11 @@ class Springboard : Item {
         if (player == null){return net.minecraft.util.EnumActionResult.FAIL} // check for null
 
         if (player.onGround){
+            player.playSound(SoundEvents.BLOCK_PISTON_EXTEND, .7f, 1f)
+            player.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, .7f, 1f)
+            player.playSound(SoundEvents.ENTITY_FIREWORK_ROCKET_SHOOT, .5f, 1f)
             player.addVelocity(0.0,VELOCITY,0.0) // wheeeee
-            return net.minecraft.util.EnumActionResult.SUCCESS
+            player.cooldownTracker.setCooldown(this, 20)
         }
         return net.minecraft.util.EnumActionResult.PASS
     }
