4a546f627de8c07f85ea55134e28aa8c66d44ce9
Tinzin <tiffany.tinzin@gmail.com>
2018-09-10 02:51:57 -0700

    why doesn't this work

---
+++ src/main/java/net/tinzin/relocate/mixin/MixinTooQuick.java
@@ -0,0 +1,29 @@
+package net.tinzin.relocate.mixin;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(NetHandlerPlayServer.class)
+public class MixinTooQuick {
+    @Redirect(method = "processPlayer",
+    at=@At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;isElytraFlying()Z", ordinal = 0),
+    require = 1)
+    boolean elyRedirect(EntityLivingBase e){
+        return true;
+    }
+
+    @Redirect(method = "processVehicleMove",
+    at=@At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isSinglePlayer()Z", ordinal = 0))
+    boolean spRedirect(MinecraftServer minecraftServer){
+        return true;
+    }
+    @Redirect(method = "processVehicleMove",
+    at=@At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 0))
+    boolean strRecdirect(String s, Object o){
+        return true;
+    }
+}
